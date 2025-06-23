751cd4b8320899032fa4e01b57678f9869247857
Khusbo Alam <khusbo.alam@neoscoder.com>
2025-06-23 11:11:08 +0600

    Table creation

---
+++ src/app/components/table/table.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const table = () =>{
+    return(
+
+    <div className="p-4">
+        <div className="p-4 border-4 outline-blue-950 rounded-lg">
+
+
+        </div>
+
+
+    </div>
+
+)};
+
+export default table;
