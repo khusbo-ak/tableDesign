f36202a984844c5cf909fd47e39c0c0d4e4f64b1
Khusbo Alam <khusbo.alam@neoscoder.com>
2025-06-25 16:52:02 +0600

    modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,20 @@ import { FiEdit } from "react-icons/fi";
 import { FaRegTrashAlt, FaSearch } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import { IoMdPrint } from "react-icons/io";
- type FoodData = {
+import { Button, Checkbox, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure, Input } from "@heroui/react";
+type FoodData = {
   id: number;
   Name: string;
-  Address: string;    
+  Address: string;
   PostCode: string;
   Rating: number;
   TypeOfFood: string;
-}; 
+};
 export default function Home() {
   const [foodData, setData] = useState<FoodData[]>([]);
   const [search, setSearch] = useState<FoodData[]>([]);
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
   useEffect(() => {
     fetch("/foodData.json")
       .then((res) => res.json())
@@ -28,74 +31,79 @@ export default function Home() {
 
   console.log(foodData);
 
-const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  let query = e.target.value;
-  const newsearch = foodData.filter(item => item.Name.toLowerCase().includes(query.toLowerCase()));
-  setSearch(newsearch);
-  if (query === "" ) {
-   console.log("No Data Found")
-    
-  }
-
-  
-
-}
-console.log("search",search)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let query = e.target.value;
+    const newsearch = foodData.filter((item) =>
+      item.Name.toLowerCase().includes(query.toLowerCase())
+    );
+    setSearch(newsearch);
+    // search.length===0 && alert("No results found");
+  };
+  console.log("search", search);
   return (
     <section className=" min-h-screen bg-gray-100">
       <div className="p-4 flex justify-end">
         <div className="p-2 flex justify-center gap-2">
-             <form className="w-80  relative ">
-               <div className="relative">
-                 <input
-                   type="search"
-                   placeholder="Search Here..."
-                   onChange={handleChange}
-                   className=" w-full h-10 p-4 border-gray-300 border-2 rounded-md text-black"/>
-               </div>
-             </form>
-             <button className="flex justify-center p-3 bg-sky-700 text-white rounded-md">
-               <FaSearch/>
-             </button>
-             <button className="flex justify-center p-3 bg-sky-700 text-white rounded-md">
-               <IoMdPrint/>
-             </button>
-           </div>
+          <form className="w-80  relative ">
+            <div className="relative">
+              <input
+                type="search"
+                placeholder="Search Here..."
+                onChange={handleChange}
+                className=" w-full h-10 p-4 border-gray-300 border-2 rounded-md text-black"
+              />
+            </div>
+          </form>
+          <button className="flex justify-center p-3 bg-sky-700 text-white rounded-md">
+            <FaSearch />
+          </button>
+          <button className="flex justify-center p-3 bg-sky-700 text-white rounded-md">
+            <IoMdPrint />
+          </button>
+        </div>
       </div>
       <div className="w-full flex justify-center">
-      <table className="w-full mx-10">
-        <thead>
-          <tr className="h-10 text-blue-400">
-            <th className=" text-start "> Name</th>
-            <th className=" text-start"> Address</th>
-            <th className=" text-start"> Postcode</th>
-            <th className=" text-start"> Rating</th>
-            <th className=" text-start"> Type of food</th>
-            <th className=" text-start  text-black "> Action</th>
-          </tr>
-        </thead>
-        <tbody className="content-center">
-          {search.map((data) => (
-            <tr
-              key={data.id}
-              className="border-t-2 h-10 border-gray-300 bg-gray-200 text-black w-60"
-            >
-              <td className="text-start">{data.Name}</td>
-              <td className="text-start"> {data.Address}</td>
-              <td className="text-start">{data.PostCode}</td>
-              <td className="text-start"> {data.Rating}</td>
-              <td className="text-start">{data.TypeOfFood}</td>
-              <td className="p-2 flex justify-start gap-2">
-                <button className="flex justify-center p-2 bg-sky-700 text-white rounded-md">
-                  <FiEdit />
-                </button>
-                <button className="flex justify-center p-2 bg-red-600 text-white rounded-md">
-                  <FaRegTrashAlt />
-                </button>{" "}
-              </td>
+        <table className="w-full mx-10">
+          <thead>
+            <tr className="h-10 text-blue-400">
+              <th className=" text-start "> Name</th>
+              <th className=" text-start"> Address</th>
+              <th className=" text-start"> Postcode</th>
+              <th className=" text-start"> Rating</th>
+              <th className=" text-start"> Type of food</th>
+              <th className=" text-start  text-black "> Action</th>
             </tr>
-          ))}
-          {/* <tr className="border-t-2 h-10 border-gray-300 bg-gray-200 text-black w-60 ">
+          </thead>
+          <tbody className="content-center">
+            {search && search.length > 0 ? (
+              search.map((data) => (
+                <tr
+                  key={data.id}
+                  className="border-t-2 h-10 border-gray-300 bg-gray-200 text-black w-60"
+                >
+                  <td className="text-start">{data.Name}</td>
+                  <td className="text-start">{data.Address}</td>
+                  <td className="text-start">{data.PostCode}</td>
+                  <td className="text-start">{data.Rating}</td>
+                  <td className="text-start">{data.TypeOfFood}</td>
+                  <td className="p-2 flex justify-start gap-2">
+                    <button onClick={onOpen} className="flex justify-center p-2 bg-sky-700 text-white rounded-md">
+                      <FiEdit />
+                    </button>
+                    <button className="flex justify-center p-2 bg-red-600 text-white rounded-md">
+                      <FaRegTrashAlt />
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td className="text-center py-4 text-gray-800">
+                  No data found
+                </td>
+              </tr>
+            )}
+            {/* <tr className="border-t-2 h-10 border-gray-300 bg-gray-200 text-black w-60 ">
               <td className="text-start"> CN Chinese </td>
               <td className="text-start"> 228 City Road</td>
               <td className="text-start"> 3JH</td>
@@ -110,7 +118,7 @@ console.log("search",search)
                 </button>{" "}
               </td>
             </tr> */}
-          {/* <tr className=" w-60 border-t-2 h-10 border-gray-300 text-black">
+            {/* <tr className=" w-60 border-t-2 h-10 border-gray-300 text-black">
               <td className="text-start">007 Takeaway </td>
               <td className="text-start">6 Drummond Street </td>
               <td className="text-start">1HY </td>
@@ -151,9 +159,49 @@ console.log("search",search)
                 <button className="flex justify-center p-2 bg-sky-700 text-white rounded-md"><FiEdit/></button>
                 <button className="flex justify-center p-2 bg-red-600 text-white rounded-md"><FaRegTrashAlt/></button></td>
             </tr> */}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
       </div>
+       <Modal className="text-black" isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Edit Information</ModalHeader>
+              <ModalBody>
+               <Input
+                 label="Name"
+                //  placeholder="Enter your Name"
+                 variant="bordered"
+               />
+                <Input
+                 label="Address"
+                 variant="bordered"
+               /> <Input
+                 label="Post Code"
+                 variant="bordered"
+               /> <Input
+                 label="Rating"
+                 variant="bordered"
+               /> <Input
+                 label="Type of food"
+                 variant="bordered"
+               />
+                {/* <div className="flex py-2 px-1 justify-between">
+                </div> */}
+              </ModalBody>
+              
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </section>
   );
 }
